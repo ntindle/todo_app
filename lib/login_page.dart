a44bf56ff117abafeed1c88b753424e9f9b07f00
Nicholas Tindle <nicktindle@outlook.com>
2022-09-16 01:51:38 -0500

    feat: account creation and login (kinda)

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 import 'package:motor_flutter/motor_flutter.dart';
-import 'package:todo_app/create_account_page.dart';
 import 'package:todo_app/main.dart';
 
 class LoginPage extends StatefulWidget {
@@ -16,24 +15,19 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  TextEditingController nameController = TextEditingController();
+  TextEditingController addressController =
+      TextEditingController(text: Get.find<MotorFlutter>().didUrl.toString());
   TextEditingController passwordController = TextEditingController();
 
-  void _handleLoginResponse(LoginResponse? resp) {
-    if (resp != null && resp.success) {
-      // Login successful
-      print(resp);
+  void _login() async {
+    print("logging in");
+    final res = await Get.find<MotorFlutter>()
+        .login(password: passwordController.text, did: addressController.text);
+    if (res != null) {
+      print(res);
     } else {
-      // Login failed
+      print('Login failed');
     }
-  }
-
-  void _login() {
-    setState(() {
-      // TODO: Add login logic
-      print(nameController.text);
-      print(passwordController.text);
-    });
   }
 
   @override
@@ -60,11 +54,11 @@ class _LoginPageState extends State<LoginPage> {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
-                controller: nameController,
+                controller: addressController,
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(),
-                    labelText: 'SNR Address',
-                    hintText: 'Enter valid wallet address as snr12kj....adsf'),
+                    labelText: 'SNR DID Address',
+                    hintText: 'Enter valid DID as did:snr:aksldjfenkjaof29oi'),
               ),
             ),
             Padding(
@@ -97,37 +91,25 @@ class _LoginPageState extends State<LoginPage> {
               height: 50,
               width: 200,
               margin: const EdgeInsets.only(top: 20),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.grey,
-                ),
-                onPressed: () => Get.to(() =>
-                    const CreateAccountPage(title: "Create Account Page")),
-                child: const Text(
-                  'Create Account',
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
+              child: MaterialButton(
+                color: Colors.white,
+                child: const Text('Register Account'),
+                // --
+                // 2. Add the RegisterAccount ModalView on tap
+                // --
+                onPressed: () async {
+                  Get.put(RegisterController());
+                  final res =
+                      await MotorFlutter.to.showRegisterModal(onError: (err) {
+                    Get.snackbar("Error", err.toString());
+                  });
+                  if (res != null) {
+                    // Store the address in the controller???
+                    Get.offAll(() => const LoginPage(title: "Login"));
+                  }
+                },
               ),
             ),
-            MaterialButton(
-              color: Colors.black,
-              child: const Text('Register Account'),
-              // --
-              // 2. Add the RegisterAccount ModalView on tap
-              // --
-              onPressed: () async {
-                Get.put(RegisterController());
-                final res =
-                    await MotorFlutter.to.showRegisterModal(onError: (err) {
-                  Get.snackbar("Error", err.toString());
-                });
-                if (res != null) {
-                  Get.offAll(() => const MyHomePage(
-                        title: "Hello",
-                      ));
-                }
-              },
-            )
           ],
         ),
       ),
